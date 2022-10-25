8f5eca34dbca6831ef19d56090af62731914e613
Alejandro Blanco <abg_blanco@hotmail.com>
2022-10-25 12:37:51 +0200

    Hola Mundo con Js 2

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,14 @@
 
-alert("Hello World!!");
+function create(htmlStr) {
+    var frag = document.createDocumentFragment(),
+        temp = document.createElement('div');
+    temp.innerHTML = htmlStr;
+    while (temp.firstChild) {
+        frag.appendChild(temp.firstChild);
+    }
+    return frag;
+}
+
+var fragment = create('<div>Hello!</div><p>...</p>');
+// You can use native DOM methods to insert the fragment:
+document.body.insertBefore(fragment, document.body.childNodes[0]);
