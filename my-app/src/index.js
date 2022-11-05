1413931a6aa59d94f6fc648d796b12625a486753
Alejandro Blanco <abg_blanco@hotmail.com>
2022-11-05 17:42:44 +0100

    xd

---
+++ my-app/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+function Hola(){
+    return <h1> adios </h1>
+}
+
+root.render(
+    <Hola/>
+)
