2436308e24a1203b7f42a9844a10dca9ed706ab5
Alejandro Blanco <abg_blanco@hotmail.com>
2022-11-07 22:51:47 +0100

    Aprendiendo como buen csm

--- my-app/src/Cell.jsx
+++ my-app/src/Cell.jsx
@@ -1,20 +1,18 @@
-import "./style.css";
-
-var turno = true;
 const player1 = "https://cdn-icons-png.flaticon.com/512/32/32341.png";
-const player2 = "https://icones.pro/wp-content/uploads/2021/08/icone-x-noir.png";
+const player2 =
+  "https://icones.pro/wp-content/uploads/2021/08/icone-x-noir.png";
 
-function Cell() {
+function Cell({ turn, setTurn }) {
   return (
     <img
       className="cell"
-      
       onClick={function check(e) {
+        console.log(turn);
         const btn = e.target;
-        if (btn.src === ""){
-          btn.src = turno ? player1 : player2;
-          turno = !turno;
+        if (btn.src === "") {
+          btn.src = turn ? player1 : player2;
         }
+        setTurn(!turn);
       }}
     ></img>
   );
--- my-app/src/Tictac.jsx
+++ my-app/src/Tictac.jsx
@@ -1,18 +1,24 @@
-import Cell from './Cell'
+import Cell from "./Cell";
+import { useState } from "react";
+
 function Tictac() {
+  var [turn, setTurn] = useState(true);
   return (
-    <div className="board">
-      <Cell />
-      <Cell />
-      <Cell />
-      <Cell />
-      <Cell />
-      <Cell />
-      <Cell />
-      <Cell />
-      <Cell />
-    </div>
+    <>
+      <div className="board">
+        <Cell turn={turn} setTurn={setTurn} />
+        <Cell turn={turn} setTurn={setTurn} />
+        <Cell turn={turn} setTurn={setTurn} />
+        <Cell turn={turn} setTurn={setTurn} />
+        <Cell turn={turn} setTurn={setTurn} />
+        <Cell turn={turn} setTurn={setTurn} />
+        <Cell turn={turn} setTurn={setTurn} />
+        <Cell turn={turn} setTurn={setTurn} />
+        <Cell turn={turn} setTurn={setTurn} />
+      </div>
+      <center> Es el turno del Jugador {turn ? 1 : 2}</center>
+    </>
   );
 }
 
-export default Tictac
+export default Tictac;
